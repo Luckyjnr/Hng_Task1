ad49d994f99ceaf32b08c161a71ec05cf52f795c
Luckyjnr <Luckykelimu@gmail.com>
2023-09-07 11:10:34 -0700

    latest edit

--- Hng_project/urls.py
+++ Hng_project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from core.views import get_endpoint_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include('core.urls')),
+    path('api/', get_endpoint_data, name='get_endpoint_data')
 ]
--- core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import get_endpoint_data
-
-urlpatterns = [
-    path('api/', get_endpoint_data, name='get_endpoint_data'),
-]
